7c42b66a04c24403bad28a5e865c7be0b465803d
Tony Hung <tbass134@gmail.com>
2017-11-10 12:23:22 -0500

    init

---
+++ test.py
@@ -0,0 +1,7 @@
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+import matplotlib.pyplot as plt
+[Fs, x] = audioBasicIO.readAudioFile("pills/pill_1.wav");
+F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR'); 
+plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy'); plt.show()
